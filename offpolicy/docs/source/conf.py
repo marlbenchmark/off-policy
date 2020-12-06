38fef64978899e33086abaab8a6cd5930a4b86db
zoeyuchao <1625360790@qq.com>
2020-12-07 01:37:08 +0800

    add docs

---
+++ offpolicy/docs/source/conf.py
@@ -0,0 +1,92 @@
+# Configuration file for the Sphinx documentation builder.
+#
+# This file only contains a selection of the most common options. For a full
+# list see the documentation:
+# https://www.sphinx-doc.org/en/master/usage/configuration.html
+
+# -- Path setup --------------------------------------------------------------
+
+# If extensions (or modules to document with autodoc) are in another directory,
+# add these directories to sys.path here. If the directory is relative to the
+# documentation root, use os.path.abspath to make it absolute, like shown here.
+#
+import os
+import sys
+sys.path.insert(0, os.path.abspath(".."))
+
+import sphinx_rtd_theme
+import sphinxcontrib
+from recommonmark.parser import CommonMarkParser
+
+
+
+# -- Project information -----------------------------------------------------
+
+project = 'offpolicy'
+copyright = '2020, Chao Yu, Akash Velu'
+author = 'Chao Yu, Akash Velu'
+
+# The full version, including alpha/beta/rc tags
+release = '0.1.0'
+
+
+# -- General configuration ---------------------------------------------------
+
+# Add any Sphinx extension module names here, as strings. They can be
+# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
+# ones.
+extensions = [
+    "sphinx_rtd_theme", # Read The Docs theme
+    'sphinx.ext.autodoc', # Automatically extract docs from docstrings
+    'sphinx.ext.doctest',
+    'sphinx.ext.intersphinx',
+    'sphinx.ext.todo',
+    'sphinx.ext.coverage', # make coverage generates documentation coverage reports
+    'sphinx.ext.mathjax',
+    'sphinx.ext.ifconfig',
+    'sphinx.ext.viewcode', # link to sourcecode from docs
+    'sphinx.ext.githubpages',
+    "sphinx.ext.napoleon",  # support Numpy and Google doc style
+    "sphinxcontrib.apidoc",
+]
+
+# configuring automated generation of api documentation
+# See: https://github.com/sphinx-contrib/apidoc
+
+apidoc_module_dir = "../.."
+apidoc_excluded_paths = ["envs", "setup.py"]
+apidoc_module_first = True
+apidoc_extra_args = [
+    "--force",
+    "--separate",
+    "--ext-viewcode",
+    "--doc-project=offpolicy",
+    "--maxdepth=2",
+    "--templatedir=_templates/apidoc",
+]
+# Add any paths that contain templates here, relative to this directory.
+templates_path = ['_templates']
+
+# List of patterns, relative to source directory, that match files and
+# directories to ignore when looking for source files.
+# This pattern also affects html_static_path and html_extra_path.
+exclude_patterns = []
+
+
+# -- Options for HTML output -------------------------------------------------
+
+# The theme to use for HTML and HTML Help pages.  See the documentation for
+# a list of builtin themes.
+#
+html_theme = 'alabaster'
+
+# Add any paths that contain custom static files (such as style sheets) here,
+# relative to this directory. They are copied after the builtin static files,
+# so a file named "default.css" will overwrite the builtin "default.css".
+html_static_path = ['_static']
+
+
+source_parsers = {
+    '.md': CommonMarkParser,
+    '.MD': CommonMarkParser,
+}
--- offpolicy/scripts/train_smac_qmix.sh
+++ offpolicy/scripts/train_smac_qmix.sh
@@ -1,14 +1,14 @@
 #!/bin/sh
 env="StarCraft2"
-map="3m"
+map="MMM2"
 algo="qmix"
-exp="debug"
+exp="icml"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 
 for seed in $(seq ${seed_max}); do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=2 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 2 --buffer_size 5000 --use_soft_update --hard_update_interval_episode 200 --num_env_steps 10000000 --gain 1 --use_feature_normlization
+    CUDA_VISIBLE_DEVICES=6 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed ${seed} --buffer_size 5000 --use_soft_update --hard_update_interval_episode 200 --num_env_steps 10000000 --gain 1 --use_feature_normlization
     echo "training is done!"
 done
