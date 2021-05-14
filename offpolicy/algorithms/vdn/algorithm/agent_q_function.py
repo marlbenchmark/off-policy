3c4a1f7eeae9b8f7366209844803b32336211ead
zoeyuchao <1625360790@qq.com>
2021-05-14 13:17:43 +0800

    fix

---
+++ offpolicy/algorithms/vdn/algorithm/agent_q_function.py
@@ -0,0 +1,51 @@
+import copy
+import numpy as np
+
+import torch
+import torch.nn as nn
+
+from offpolicy.utils.util import init, check
+from offpolicy.algorithms.utils.rnn import RNNBase
+from offpolicy.algorithms.utils.act import ACTLayer
+
+class AgentQFunction(nn.Module):
+    # GRU implementation of the Agent Q function
+
+    def __init__(self, args, input_dim, act_dim, device):
+        # input dim is agent obs dim + agent acf dim
+        # output dim is act dim
+        super(AgentQFunction, self).__init__()
+        self._use_orthogonal = args.use_orthogonal
+        self.hidden_size = args.hidden_size
+        self._gain = args.gain
+        self.device = device
+        self.tpdv = dict(dtype=torch.float32, device=device)
+
+        self.rnn = RNNBase(args, input_dim)
+
+        self.q = ACTLayer(act_dim, self.hidden_size, self._use_orthogonal, gain=self._gain)
+
+        self.to(device)
+
+    def forward(self, obs, rnn_states):
+        # make sure input is a torch tensor
+        obs = check(obs).to(**self.tpdv)
+        rnn_states = check(rnn_states).to(**self.tpdv)
+
+        no_sequence = False
+        if len(obs.shape) == 2:
+            # this means we're just getting one output (no sequence)
+            no_sequence = True
+            obs = obs[None]
+            # obs is now of shape (seq_len, batch_size, obs_dim)
+        if len(rnn_states.shape) == 2:
+            # hiddens should be of shape (1, batch_size, dim)
+            rnn_states = rnn_states[None]
+
+        inp = obs
+
+        rnn_outs, h_final = self.rnn(inp, rnn_states)
+        # pass outputs through linear layer
+        q_outs = self.q(rnn_outs, no_sequence)
+
+        return q_outs, h_final
--- offpolicy/scripts/train_smac_qmix.sh
+++ offpolicy/scripts/train_smac_qmix.sh
@@ -1,15 +1,15 @@
 #!/bin/sh
 env="StarCraft2"
-map="27m_vs_30m"
+map="3s_vs_3z"
 algo="qmix"
-exp="originalstate_gain1"
+exp="global_alllocal"
 seed_max=1
 
 echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is ${seed_max}"
 
 for seed in $(seq ${seed_max}); do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 8 --buffer_size 5000 --lr 5e-4 --batch_size 32 --use_soft_update --hard_update_interval_episode 200 --num_env_steps 10000000 --log_interval 80000 --eval_interval 80000 --user_name "zoeyuchao" --gain 1
+    CUDA_VISIBLE_DEVICES=2 python train/train_smac.py --env_name ${env} --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} --seed 3 --buffer_size 5000 --lr 5e-4 --batch_size 32 --use_soft_update --hard_update_interval_episode 200 --num_env_steps 5000000 --log_interval 3000 --eval_interval 20000 --user_name "zoeyuchao"
     echo "training is done!"
 done
 
