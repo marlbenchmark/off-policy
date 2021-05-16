6eb4a35e82dbc0cd89280d9a6653f50430848641
zoeyuchao <1625360790@qq.com>
2021-05-16 21:34:27 +0800

    update

--- offpolicy/scripts/train_smac_qmix.sh
+++ offpolicy/scripts/train_smac_qmix.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 env="StarCraft2"
-map="3s_vs_3z"
+map="3s5z_vs_3s6z"
 algo="qmix"
 exp="global_alllocal"
 seed_max=1
@@ -9,12 +9,12 @@ echo "env is ${env}, map is ${map}, algo is ${algo}, exp is ${exp}, max seed is 
 
 for seed in $(seq ${seed_max}); do
     echo "seed is ${seed}:"
-    CUDA_VISIBLE_DEVICES=2 python train/train_smac.py --env_name ${env}\
-     --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map}\
-      --seed 3 --buffer_size 5000 --lr 5e-4 --batch_size 32 --use_soft_update\
-       --hard_update_interval_episode 200 --num_env_steps 5000000 \
+    CUDA_VISIBLE_DEVICES=1 python train/train_smac.py --env_name ${env} \
+     --algorithm_name ${algo} --experiment_name ${exp} --map_name ${map} \
+      --seed 1 --n_training_threads 128 --buffer_size 5000 --lr 5e-4 --batch_size 32 --use_soft_update \
+       --hard_update_interval_episode 200 --num_env_steps 10000000 \
        --log_interval 3000 --eval_interval 20000 --user_name "zoeyuchao"\
-       --use_global_all_local_state
+       --use_global_all_local_state --gain 1
     echo "training is done!"
 done
 
